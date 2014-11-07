3b03a1a762a7addaacdc04ee272036d17dcba0d7
huyanping <hypxm@qq.com>
2014-11-07 09:43:03 +0800

    init

---
+++ src/Jenner/Zebra/Tools/CommonArray.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 14-11-7
+ * Time: 上午9:41
+ */
+
+namespace Jenner\Zebra\Tools;
+
+
+class CommonArray {
+    /**
+     * 根据列的Key获取一个二维数组中某一列的值，作为一维数组返回
+     * @param $column
+     * @param $array
+     * @return array
+     */
+    public static function columnValue($column, $array){
+        $result = [];
+        foreach($array as $value){
+            $result[] = empty($value[$column]) ? null : $value[$column];
+        }
+        return $result;
+    }
+} 
