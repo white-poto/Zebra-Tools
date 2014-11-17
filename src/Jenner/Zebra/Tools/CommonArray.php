6ff707a25f22bb546c0ffc2fd3d502f55825a780
huyanping <hypxm@qq.com>
2014-11-17 13:51:23 +0800

    add http

--- src/Jenner/Zebra/Tools/CommonArray.php
+++ src/Jenner/Zebra/Tools/CommonArray.php
@@ -67,4 +67,17 @@ class CommonArray {
         \call_user_func_array('array_multisort', $args);
         return \array_pop($args);
     }
+
+    /**
+     * object 转 array
+     */
+    function objectToArray($obj){
+        $_arr = is_object($obj)? get_object_vars($obj) : $obj;
+        foreach ($_arr as $key => $val) {
+            $val = (is_array($val)) || is_object($val) ? object_to_array($val) : $val;
+            $arr[$key] = $val;
+        }
+
+        return $arr;
+    }
 } 
