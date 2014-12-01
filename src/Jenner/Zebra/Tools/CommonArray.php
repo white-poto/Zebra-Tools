addb7199cc2553906067a748b31b5a39c2e4f5f1
huyanping <hypxm@qq.com>
2014-12-01 10:11:39 +0800

    update

--- src/Jenner/Zebra/Tools/CommonArray.php
+++ src/Jenner/Zebra/Tools/CommonArray.php
@@ -9,10 +9,10 @@
 namespace Jenner\Zebra\Tools;
 
 
-/**
- * Class CommonArray
- * @package Jenner\Zebra\Tools
- */
+    /**
+     * Class CommonArray
+     * @package Jenner\Zebra\Tools
+     */
 /**
  * Class CommonArray
  * @package Jenner\Zebra\Tools
@@ -89,6 +89,30 @@ class CommonArray {
 
 
     /**
+     * 将一个二维数组，以其中一列为KEY，一列为VALUE，返回一个二维数组
+     * @param $array
+     * @param $array_key_key
+     * @param $array_value_key
+     * @return array
+     */
+    public static function tableToArrayMapping(array $array, $array_key_key, $array_value_key=null){
+        $result = [];
+        foreach($array as $arr){
+            if(!is_array($arr)) continue;
+            $key = $arr[$array_key_key];
+            if(is_null($array_value_key)){
+                $value = $arr;
+            }else{
+                $value = $arr[$array_value_key];
+            }
+            //区别在这里
+            $result[$key][] = $value;
+        }
+
+        return $result;
+    }
+
+    /**
      * 将一个二维数组，以其中一列为KEY，一列为VALUE，返回一个一维数组
      * @param $array
      * @param $array_key_key
@@ -105,10 +129,61 @@ class CommonArray {
             }else{
                 $value = $arr[$array_value_key];
             }
+            //区别在这里
             $result[$key] = $value;
         }
 
         return $result;
+    }
+
+    /**
+     * 填充数组下标，下标格式为日志格式('Y-m-d')
+     * @param array $array
+     * @param $start_date
+     * @param $end_date
+     * @param $step
+     * @param $fill_date
+     * @throws \Exception
+     * @return array
+     */
+    public static function fillArrayDateNumKey(array $array, $start_date, $end_date, $step, $fill_date){
+        if(!strtotime($start_date) || !strtotime($end_date))
+            throw new \Exception('date format error. right format is "Y-m-d"');
+        if(!in_array($step, ['day', 'week', 'month', 'year']))
+            throw new \Exception('step param error. it should be day, week, month or year');
+
+        while(strtotime($start_date)<strtotime($end_date)){
+            if(!isset($array[$start_date])){
+                $array[$start_date] = $fill_date;
+            }
+            $start_date = date("Y-m-d", strtotime($start_date . " + 1 {$step}"));
+        }
+        return $array;
+    }
+
+    /**
+     * 填充数组下标，下标格式为日志格式('Y-m-d H:i:s')
+     * @param array $array
+     * @param $start_time
+     * @param $end_time
+     * @param $step
+     * @param $fill_date
+     * @return array
+     * @throws \Exception
+     */
+    public static function fillArrayDateTimeNumKey(array $array, $start_time, $end_time, $step, $fill_date){
+        if(!strtotime($start_time) || !strtotime($end_time))
+            throw new \Exception('date format error. right format is "Y-m-d H:i:s"');
+        if(!in_array($step, ['day', 'week', 'month', 'year']))
+            throw new \Exception('step param error. it should be day, week, month or year');
+
+        while(strtotime($start_time)<strtotime($end_time)){
+            if(!isset($array[$start_time])){
+                $array[$start_time] = $fill_date;
+            }
+            $start_time = date("Y-m-d H:i:s", strtotime($start_time . " + 1 {$step}"));
+        }
+        return $array;
     }
 
     /**
@@ -118,15 +193,18 @@ class CommonArray {
      * @param array $array
      * @param $end
      * @param int $start
-     * @param $fill_value
+     * @param int $step
+     * @param int $fill_value
      * @return array
      */
-    public static function fillArrayNumKey(array $array, $end, $start=0, $fill_value){
-        for(;$start<$end;$start++){
+    public static function fillArrayNumKey(array $array, $end, $start=0, $step=1, $fill_value=0){
+        for(;$start<$end;$start=$start+$step){
             if(!isset($array[$start])){
                 $array[$start] = $fill_value;
             }
         }
+
+        ksort($array);
 
         return $array;
     }
